01c0df1c48278fd567bf2861e806fb4ccd066369
valeriacruise <valeriacruise22@gmail.com>
2018-07-10 20:09:36 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -10,7 +10,7 @@ STUDENT_NAMES = [
 def first_student_by_index
   # Write a solution that returns the first student in the array student_names
   student_names= ["Adele", "Beyoncé", "Cardi B", "Lady Gaga", "Nicki Minaj", "Rihanna"]
-room.first
+first_student_by_index.first
 end
 
 first_student_by_index
